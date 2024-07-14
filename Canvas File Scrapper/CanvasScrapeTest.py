4a562b58bb09db7693f285f4d21f42896351472d
Pengu601 <111535906+Pengu601@users.noreply.github.com>
2024-07-13 21:55:26 -0400

    canvas scrapper

---
+++ Canvas File Scrapper/CanvasScrapeTest.py
@@ -0,0 +1,40 @@
+import requests
+
+from operations import *
+#headers used to get access to user account
+headers = {
+    'Authorization': 'Bearer 1158~WVPhPyWU4Qve4ZwB9rFeBJkLG7Zy2L8u6FLFmwtxHvJDWhEVQmTUXNwEyRaJWVEr',
+}
+#params used to filter for specific info 
+params = {
+    'enrollment_state' : 'active', #used to only get courses that are currently active
+}
+
+# def getDueDates(courseID):
+#     request = requests.get("https://webcourses.ucf.edu/api/v1/courses/" + str(courseID) + "/permissions", headers=headers)
+#     data = request.json()
+#     for i in data:
+#         print(i)
+
+
+#Todo - make the header parameter a user input
+response = requests.get('https://webcourses.ucf.edu/api/v1/users/self/favorites/courses', headers=headers, params = params)
+
+#check if authentication is successfull
+print(response.status_code)
+print()
+
+data = response.json() #turn the response into a json object to be able to store data
+
+courses = [] #stores course id
+for i in data:
+    courses.append(i['id'])
+    
+# for course in courses:
+#     print(course)
+#     getDueDates(course)
+#     print()
+
+downloadCourseFiles(courses[0])
+
+
---
+++ Canvas File Scrapper/operations.py
@@ -0,0 +1,26 @@
+import requests
+from tkinter import Tk
+from tkinter.filedialog import askdirectory
+
+
+headers = {
+    'Authorization': 'Bearer 1158~WVPhPyWU4Qve4ZwB9rFeBJkLG7Zy2L8u6FLFmwtxHvJDWhEVQmTUXNwEyRaJWVEr',
+}
+
+def getDueDates(courseID):
+    request = requests.get("https://webcourses.ucf.edu/api/v1/courses/" + str(courseID) + "/permissions", headers=headers)
+    data = request.json()
+    for i in data:
+        print(i)
+        
+def downloadCourseFiles(courseID):
+    #get directory path of where user wants to download course files
+    filePath = askdirectory(title='Select Folder') #gets folder path for assignments to download to
+    print(filePath)
+    
+    request = requests.get(f'https://webcourses.ucf.edu/api/v1/courses/{courseID}/content_exports', headers=headers, params={'export_type': 'zip', 'select': 'files'})
+    data = request.json()
+    
+    print(data)
+    
+    
