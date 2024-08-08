0b313325808dd7b07d5c635c451cf6e8e83034d3
Pengu601 <111535906+Pengu601@users.noreply.github.com>
2024-08-08 01:34:17 -0400

    Download file from URL to directory

--- Canvas File Scrapper/operations.py
+++ Canvas File Scrapper/operations.py
@@ -2,6 +2,7 @@ import requests
 from tkinter import Tk
 from tkinter.filedialog import askdirectory
 import time
+import os
 
 headers = {
     'Authorization': 'Bearer 1158~WVPhPyWU4Qve4ZwB9rFeBJkLG7Zy2L8u6FLFmwtxHvJDWhEVQmTUXNwEyRaJWVEr',
@@ -20,42 +21,48 @@ def downloadCourseFiles(courseID):
     
     #get request to download content from course
     request = requests.post(f'https://webcourses.ucf.edu/api/v1/courses/{courseID}/content_exports', headers=headers, params= {'export_type': 'zip'} )
-    print(request.status_code)
+    # print(request.status_code)
     
     data = request.json()
     print(data)
     contentID = data['id']
     print(contentID) #
     
+    #Gets progress for Content Download
     progressURL = data['progress_url']
     progressID = progressURL.rsplit('/', 1)[-1]
-    # #gets progress of content download - todo
     
-    # contentProgress = data['progress_url']
-    # showExportRequest = requests.get(contentProgress, headers=headers)
-    # # print(showExportRequest.status_code)
-    # print(showExportRequest)
-    # # data1 = showExportRequest.json()
-    # # print(data1)
-    
-    progressBar = 0.0
+    progressBar = 0
     progressRequestRAW = requests.get(f'https://webcourses.ucf.edu/api/v1/progress/{progressID}', headers = headers)
-    print(progressRequestRAW.status_code)
+    # print(progressRequestRAW.status_code)
     progressRequest = progressRequestRAW.json()
 
     print(progressRequest)
-    while(progressBar != 100.0):
+    #Until Download Completes, update progress completion
+    while(progressBar != 100):
+        time.sleep(2)
         progressRequestRAW = requests.get(f'https://webcourses.ucf.edu/api/v1/progress/{progressID}', headers = headers)
         progressRequest = progressRequestRAW.json()
         progressBar = progressRequest['completion']
-        print(progressBar)
-        time.sleep(2)
+        print(str(progressBar) + '%')
+        
     
     
     #gets url for content download
-    contentURL = requests.get(f'https://webcourses.ucf.edu/api/v1/courses/{courseID}/content_exports/{contentID}', headers=headers)
-    print(contentURL.status_code)
-    data3 = contentURL.json()
-    print(data3)
+    contentURLRAW = requests.get(f'https://webcourses.ucf.edu/api/v1/courses/{courseID}/content_exports/{contentID}', headers=headers)
+    # print(contentURL.status_code)
+    contentURL = contentURLRAW.json()
+    print(contentURL)
+    
+    downloadURL = contentURL['attachment']['url']
+    fileName = contentURL['attachment']['filename']
+    savePath = os.path.join(filePath, fileName)
+    
+    downloadContent = requests.get(downloadURL)
+    #Download File from URL and save to Directory
+    with open(savePath, 'wb') as file:
+        file.write(downloadContent.content)
+    
+    
     
     
