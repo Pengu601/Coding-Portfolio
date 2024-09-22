1bc41a49ce7309deca47fd986c5fe174293def60
Pengu601 <111535906+Pengu601@users.noreply.github.com>
2024-09-22 13:44:04 -0400

    test

--- Canvas File Scrapper/menu.py
+++ Canvas File Scrapper/menu.py
@@ -1,9 +1,18 @@
-import requests
-
-from authentication import *
+from operations import *
 
 def menu():
-    while(1):
-        print('Welcome to UCF Canvas Helper')
-        print('-----------------------------')
-        
+    print("--------------------------------")
+    print("2. TBD")    
+    print("3. TBD")
+    print("4. TBD")    
+    print("--------------------------------")
+    choice = input(">> ")
+    print(choice)
+    if(choice == 1)
+        downloadCourseFiles(course[0])
+    
+def main():
+    menu()
+    
+if __name__ == "__main__":
+    main()
